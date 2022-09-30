5246b6a8d9c74d6be28d5daa1c6d1e45ee2921aa
Joanne Santhosh <joanne.santhosh@mths.ca>
2022-09-30 00:56:07 +0000

    update

---
+++ circumference_of_circle.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2022 Joanne Santhosh All rights reserved
+//
+// Created by: Joanne Santhosh
+// Created on: Sept 2022
+// This program calculates the circumference of a circle
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+    // This function calculates the circumference
+    const double TAU = 6.2831853071;
+    int radiusOfCircle;
+    double CircumferenceOfCircle;
+
+    // input
+    std::cout << "Enter the radius of the circle (mm): ";
+    std::cin >> radiusOfCircle;
+
+    // process
+    CircumferenceOfCircle = TAU * radiusOfCircle;
+
+    // output
+    std::cout << "" << std::endl;
+    std::cout << "The circumference is " << CircumferenceOfCircle << " mm." << std::endl; 
+    std::cout << "\nDone." << std::endl;
+}
